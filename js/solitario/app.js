83b8cf7ec73db3be805969b72208718a8a2d7a0a
Ofir Picazo <ofirpicazo@gmail.com>
2012-11-06 21:42:02 -0800

    Adds initial Closure support.

---
+++ js/solitario/app.js
@@ -0,0 +1,12 @@
+// Copyright 2012 Ofir Picazo. All Rights Reserved
+
+goog.provide('solitario.init');
+
+/**
+ * Start point for the application.
+ */
+solitario.init = function() {
+
+};
+
+goog.exportSymbol('solitario.init', solitario.init);
