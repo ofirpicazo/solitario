c74d34673d4ed463e1c5698f7b0a69824455633c
Ofir Picazo <ofirpicazo@gmail.com>
2012-11-06 19:55:41 -0800

    Changes layout to be absolute and creates templates for all cards.

--- bin/generate_cards.py
+++ bin/generate_cards.py
@@ -8,7 +8,7 @@ SUIT_MAP = {
   'club': '♣',
   'diamond': '♦',
   'heart': '♥',
-  'spade': '♠'
+  'spade': '♠',
 }
 
 templateA = """
@@ -68,21 +68,189 @@ template3 = """
   </div>
 </div>"""
 
-CARD_TEMPLATES = {
-  'A': templateA,
-  '2': template2,
-  '3': template3,
-  # '4': template4,
-  # '5': template5,
-  # '6': template6,
-  # '7': template7,
-  # '8': template8,
-  # '9': template9,
-  # '10': template10,
-  # 'J': templateJ,
-  # 'Q': templateQ,
-  # 'K': templateK
-}
+template4 = """
+<div class="card %(suit)s" id="%(id)s">
+  <div class="flipper">
+    <div class="front">
+      <div class="corner top">
+        <span class="number">%(number)s</span>
+        <span>%(symbol)s</span>
+      </div>
+      <span class="suit top left">%(symbol)s</span>
+      <span class="suit top right">%(symbol)s</span>
+      <span class="suit bottom left">%(symbol)s</span>
+      <span class="suit bottom right">%(symbol)s</span>
+      <div class="corner bottom">
+        <span class="number">%(number)s</span>
+        <span>%(symbol)s</span>
+      </div>
+    </div>
+    <div class="back"></div>
+  </div>
+</div>"""
+
+template5 = """
+<div class="card %(suit)s" id="%(id)s">
+  <div class="flipper">
+    <div class="front">
+      <div class="corner top">
+        <span class="number">%(number)s</span>
+        <span>%(symbol)s</span>
+      </div>
+      <span class="suit top left">%(symbol)s</span>
+      <span class="suit top right">%(symbol)s</span>
+      <span class="suit middle center">%(symbol)s</span>
+      <span class="suit bottom left">%(symbol)s</span>
+      <span class="suit bottom right">%(symbol)s</span>
+      <div class="corner bottom">
+        <span class="number">%(number)s</span>
+        <span>%(symbol)s</span>
+      </div>
+    </div>
+    <div class="back"></div>
+  </div>
+</div>"""
+
+template6 = """
+<div class="card %(suit)s" id="%(id)s">
+  <div class="flipper">
+    <div class="front">
+      <div class="corner top">
+        <span class="number">%(number)s</span>
+        <span>%(symbol)s</span>
+      </div>
+      <span class="suit top left">%(symbol)s</span>
+      <span class="suit top right">%(symbol)s</span>
+      <span class="suit middle left">%(symbol)s</span>
+      <span class="suit middle right">%(symbol)s</span>
+      <span class="suit bottom left">%(symbol)s</span>
+      <span class="suit bottom right">%(symbol)s</span>
+      <div class="corner bottom">
+        <span class="number">%(number)s</span>
+        <span>%(symbol)s</span>
+      </div>
+    </div>
+    <div class="back"></div>
+  </div>
+</div>"""
+
+template7 = """
+<div class="card %(suit)s" id="%(id)s">
+  <div class="flipper">
+    <div class="front">
+      <div class="corner top">
+        <span class="number">%(number)s</span>
+        <span>%(symbol)s</span>
+      </div>
+      <span class="suit top left">%(symbol)s</span>
+      <span class="suit top right">%(symbol)s</span>
+      <span class="suit under-top center">%(symbol)s</span>
+      <span class="suit middle left">%(symbol)s</span>
+      <span class="suit middle right">%(symbol)s</span>
+      <span class="suit bottom left">%(symbol)s</span>
+      <span class="suit bottom right">%(symbol)s</span>
+      <div class="corner bottom">
+        <span class="number">%(number)s</span>
+        <span>%(symbol)s</span>
+      </div>
+    </div>
+    <div class="back"></div>
+  </div>
+</div>"""
+
+template8 = """
+<div class="card %(suit)s" id="%(id)s">
+  <div class="flipper">
+    <div class="front">
+      <div class="corner top">
+        <span class="number">%(number)s</span>
+        <span>%(symbol)s</span>
+      </div>
+      <span class="suit top left">%(symbol)s</span>
+      <span class="suit top right">%(symbol)s</span>
+      <span class="suit under-top center">%(symbol)s</span>
+      <span class="suit middle left">%(symbol)s</span>
+      <span class="suit middle right">%(symbol)s</span>
+      <span class="suit over-bottom center">%(symbol)s</span>
+      <span class="suit bottom left">%(symbol)s</span>
+      <span class="suit bottom right">%(symbol)s</span>
+      <div class="corner bottom">
+        <span class="number">%(number)s</span>
+        <span>%(symbol)s</span>
+      </div>
+    </div>
+    <div class="back"></div>
+  </div>
+</div>"""
+
+template9 = """
+<div class="card %(suit)s" id="%(id)s">
+  <div class="flipper">
+    <div class="front">
+      <div class="corner top">
+        <span class="number">%(number)s</span>
+        <span>%(symbol)s</span>
+      </div>
+      <span class="suit top left">%(symbol)s</span>
+      <span class="suit top right">%(symbol)s</span>
+      <span class="suit under-top center">%(symbol)s</span>
+      <span class="suit over-middle left">%(symbol)s</span>
+      <span class="suit over-middle right">%(symbol)s</span>
+      <span class="suit under-middle left">%(symbol)s</span>
+      <span class="suit under-middle right">%(symbol)s</span>
+      <span class="suit bottom left">%(symbol)s</span>
+      <span class="suit bottom right">%(symbol)s</span>
+      <div class="corner bottom">
+        <span class="number">%(number)s</span>
+        <span>%(symbol)s</span>
+      </div>
+    </div>
+    <div class="back"></div>
+  </div>
+</div>"""
+
+template10 = """
+<div class="card %(suit)s" id="%(id)s">
+  <div class="flipper">
+    <div class="front">
+      <div class="corner top">
+        <span class="number">%(number)s</span>
+        <span>%(symbol)s</span>
+      </div>
+      <span class="suit top left">%(symbol)s</span>
+      <span class="suit top right">%(symbol)s</span>
+      <span class="suit under-top center">%(symbol)s</span>
+      <span class="suit over-middle left">%(symbol)s</span>
+      <span class="suit over-middle right">%(symbol)s</span>
+      <span class="suit under-middle left">%(symbol)s</span>
+      <span class="suit under-middle right">%(symbol)s</span>
+      <span class="suit over-bottom center">%(symbol)s</span>
+      <span class="suit bottom left">%(symbol)s</span>
+      <span class="suit bottom right">%(symbol)s</span>
+      <div class="corner bottom">
+        <span class="number">%(number)s</span>
+        <span>%(symbol)s</span>
+      </div>
+    </div>
+    <div class="back"></div>
+  </div>
+</div>"""
+
+CARD_TEMPLATES = (
+  ('A', templateA),
+  ('2', template2),
+  ('3', template3),
+  ('4', template4),
+  ('5', template5),
+  ('6', template6),
+  ('7', template7),
+  ('8', template8),
+  ('9', template9),
+  ('10', template10),
+  ('J', templateA),
+  ('Q', templateA),
+  ('K', templateA),
+)
 
 
 def main(args):
@@ -91,7 +259,7 @@ def main(args):
                       help="Suit to create templates for")
   args = parser.parse_args(args)
 
-  for number, template in CARD_TEMPLATES.items():
+  for number, template in CARD_TEMPLATES:
     symbol = SUIT_MAP[args.suit]
     id = args.suit[0] + number.lower()  # e.g. d9 for diamond 9
     print template % {'suit': args.suit,
