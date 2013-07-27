7c7420e11700f1a047765b878350f63697e3911e
Carlos Mondragon <cadragon@gmail.com>
2013-07-27 18:13:46 -0500

    Basic gruntfile for compass compiling

---
+++ Gruntfile.js
@@ -0,0 +1,29 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    compass: {
+      dev: {
+        options: {
+          cssDir: 'css',
+          javascriptsDir: 'js',
+          imagesDir: 'images',
+          sassDir: 'src/sass',
+          fontsDir: 'fonts',
+          force: true,
+          relativeAssets: true,
+          outputStyle: 'expanded'
+        }
+      }
+    },
+    watch: {
+      files: ['src/**/*.scss'],
+      tasks: ['compass']
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-closure-tools');
+  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', ['compass', 'watch']);
+};
--- README.md
+++ README.md
@@ -3,7 +3,15 @@ solitario
 
 Solitaire HTML5 game
 
+Setup
+=====
+npm install
+./node_modules/.bin/grunt
+
+
 
 Dependencies
 ===========
 Java JRE
+sudo npm install -g grunt-cli
+gem update --system && gem install compass
